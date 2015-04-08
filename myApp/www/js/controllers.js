19a5d3587c5bda84ef622776dc8287d3f07c9713
hallworth <ian@hallworthdesign.co.uk>
2015-04-08 17:38:04 +0100

    up

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -1,10 +1,49 @@
 angular.module('starter.controllers', [])
-
-.controller('DashCtrl', function($scope) {
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+.controller('DashCtrl', function ($scope) {
+    $scope.map = {
+        center: {
+            latitude: 50.8351023,
+            longitude: -0.1720476
+        },
+        zoom: 12,
+        bounds: {}
+    };
+    $scope.options = {
+        scrollwheel: false
+    };
+    $scope.marker = {
+        id:0,
+        coords: {
+            latitude: 50.8351023,
+            longitude: -0.1720476
+        }
+    }
+    $scope.markerList = [
+        {
+            id:0,
+            latitude: 50.8351023,
+            longitude: -0.1720476
+        },
+        {
+            id:1,
+            latitude: 50.8351023,
+            longitude: -0.2720476
+        }
+    ];
     
-})
+    $scope.windowOptions = {
+        visible: false
+    };
 
+    $scope.onClick = function () {
+        $scope.windowOptions.visible = !$scope.windowOptions.visible;
+    };
+
+    $scope.closeClick = function () {
+        $scope.windowOptions.visible = false;
+    };
+
+})
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
